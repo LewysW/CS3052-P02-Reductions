1e0fcf1bea8f632c9e02f5cd76a9805b00f2b02f
locw <lewysward99@gmail.com>
2019-04-19 14:04:03 +0100

    Set up project and main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
-//
-// Created by locw on 19/04/19.
-//
-#include <iostream>
+#include "main.h"
 
-using namespace std;
+int main(int argc, char* argv[]) {
+    if (argc > 1) return 0;
+    cout << argv[0] << endl;
+    string execStr(argv[0]);
 
-int main() {
-    cout << "Hello World!\n" << endl;
+    if (endsWith(execStr, "sattothreesat")) {
+        cout << "sattothreesat" << endl;
+    } else if (endsWith(execStr, "coltosat")) {
+        cout << "coltosat" << endl;
+    } else if (endsWith(execStr, "threesattocol")) {
+        cout << "threesattocol" << endl;
+    }
+
     return 0;
+}
+
+/**
+ * Function to check whether a
+ * string has a particular suffix
+ * @param str - string to check
+ * @param suffix - suffix to check for
+ * @return - whether string has suffix
+ *
+ * source - //https://stackoverflow.com/questions/25777655/a-c-function-that-tests-if-the-c-string-ends-with-a-suffix
+ */
+bool endsWith(std::string str, std::string suffix) {
+    if (str.length() < suffix.length())
+        return false;
+
+    return str.substr(str.length() - suffix.length()) == suffix;
 }
--- src/main.h
+++ src/main.h
@@ -4,5 +4,11 @@
 
 #ifndef CS3052_P02_REDUCTIONS_MAIN_H
 #define CS3052_P02_REDUCTIONS_MAIN_H
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool endsWith(std::string str, std::string suffix);
+
 
 #endif //CS3052_P02_REDUCTIONS_MAIN_H
